4690475b71177b4f680ceb8c32d842c26e3af0a1
Zachary Starr <117234141+zachstarr03@users.noreply.github.com>
2025-07-31 22:36:47 +0000

    Currently working on...

---
+++ Solitare.py
@@ -0,0 +1,29 @@
+'''
+
+    Game Title: Solitare 
+    Created By: Zachary Starr
+    Created On: 07/31/2025
+
+    Reference material online: https://api.arcade.academy/en/2.6.7/tutorials/card_game/
+
+    Main python library used for this program will be the 'arcade' library
+
+'''
+
+import time
+import arcade
+
+#######################################################################################
+
+def main():
+
+    print("Testing testing...\n")
+
+if __name__ == "__main__":
+    main()
+
+    time.sleep(5)
+    print("Currently under development...")
+    time.sleep(3)
+    print("Pardon our dust")
+
